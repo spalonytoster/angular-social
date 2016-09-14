b8d25e7f9a1647aa8ff9cb9efec4891281054b98
Maciej Posłuszny <xmachiei@gmail.com>
2016-09-14 10:14:50 +0200

    reconnect to ws after 5s

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,14 +1,22 @@
 angular.module('app')
-  .run(function ($rootScope) {
-    var url = 'ws://' + window.location.host;
-    var connection = new WebSocket(url);
+  .run(function ($rootScope, $timeout) {
+    (function connect() {
+      'use strict';
+      var url = 'ws://' + window.location.host;
+      var connection = new WebSocket(url);
 
-    connection.onopen = function () {
-      console.log('Nawiązano połączenie z WebSocket.');
-    };
+      connection.onopen = function () {
+        console.log('Nawiązano połączenie z WebSocket.');
+      };
 
-    connection.onmessage = function (data) {
-      var message = JSON.parse(data.data);
-      $rootScope.$broadcast('ws:' + message.topic, message.data);
-    };
+      connection.onclose = function () {
+        console.log('Zamknięto połączenie z WebSocket. Próba ponownego nawiązania połączenia za 5s...');
+        $timeout(connect, 5 * 1000);
+      };
+
+      connection.onmessage = function (data) {
+        var message = JSON.parse(data.data);
+        $rootScope.$broadcast('ws:' + message.topic, message.data);
+      };
+    }());
   });
